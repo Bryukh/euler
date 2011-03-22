df9e60ceed6c49884985875e57fbeede00316ec9
Valentin Bryukhanov <bvv.mag@gmail.com>
2011-03-22 16:53:23 +1100

    first problem

---
+++ 001/problem_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000."""
+
+def sum3and5 (edge):
+    "My"
+    sum3 = sum([x*3 for x in xrange((edge-1)//3 + 1)])
+    sum5 = sum((x*5 for x in xrange((edge-1)//5 + 1)))
+    sum15 = sum((x*15 for x in xrange((edge-1)//15 + 1)))
+    return (sum3 + sum5 - sum15)
+
+def sum3and5_v2 (edge):
+    "from https://github.com/nixeagle/euler"
+    (y, rang) = (0, range(1,edge))
+    for x in rang:
+        if x % 3 == 0: y += x;
+        elif x % 5 == 0: y += x
+    return y
+
+if __name__ == "__main__":
+    print sum3and5(1000)
+    print sum3and5_v2(1000)
