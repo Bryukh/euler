dc9d1b5807ecfdf97f8688a97db0d3d2f3d51ca0
Valentin Bryukhanov <bvv.mag@gmail.com>
2011-03-26 20:30:13 +1100

    problem 48

---
+++ 048/problem048.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000."""
+import math
+
+def last_pow_numb(base, pow, last):
+    """function for last number in power"""
+    return reduce(lambda x, y: (x * y) % last, [base]*(pow-1), base)
+
+if __name__ == "__main__":
+    l = math.pow(10, 10)
+    s = sum([last_pow_numb(x, x, l) for x in xrange(1, 1001)])
+    print s % l
