71f98756bcdfddc7a0cf3c0b2be833e7697bbd76
Valentin Bryukhanov <bvv.mag@gmail.com>
2011-04-07 12:38:35 +1200

    problem 34

---
+++ 034/p034.py
@@ -0,0 +1,12 @@
+__author__ = 'duov'
+
+
+def factorial(numb):
+    return reduce(lambda x,y: x*y, range(1,numb+1)) if numb else 1
+
+s = 0
+for d in xrange(3, 10000000):
+    if sum([factorial(int(c)) for c in str(d)]) == d:
+        print d
+        s += d
+print s
